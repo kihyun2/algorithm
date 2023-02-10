3a86a92d0f31943cc6773c6386f5b2d644956ac3
kihyuny <103742790+kihyuny@users.noreply.github.com>
2023-02-10 16:14:53 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,34 +1,26 @@
 def solution(id_list, report, k):
-
-    answer = [0*m for m in range(len(id_list))]
-    report_lst = []
-    report_count = {}
-    report_dict = {}
+    answer = [0 for _ in range(len(id_list))]
     report=list(set(report))
+    report_dict={}
+
     for target in report:
-        report_lst.append(target.split())
         idx, value = target.split()
         if idx in report_dict.keys():
             report_dict[idx].append(value)
         else:
             report_dict[idx] = []
             report_dict[idx].append(value)
+    
 
-    for target in report_lst:
-        if target[1] not in report_count:
-            report_count[target[1]] = 1
-        else:
-            report_count[target[1]] += 1
-    freeze_lst = []
-    for key, value in report_count.items():
-        if value >= k:
-            freeze_lst.append(key)
-
-
-    for idx, id in enumerate(id_list):
-        if id in report_dict:
-            for freeze in freeze_lst:
-                if freeze in report_dict[id]:
-                    answer[idx] += 1
-
+    for id in id_list:
+        cnt=0
+        for key in report_dict:
+            if id in report_dict[key]:
+                cnt+=1
+        if cnt>=k:
+            for idx, id2 in enumerate(id_list):
+                if id2 in report_dict.keys():
+                    if id in report_dict[id2]:
+                        answer[idx]+=1
+    
     return answer
