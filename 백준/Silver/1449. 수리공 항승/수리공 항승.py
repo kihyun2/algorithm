b4882d3d963ef4fea7a26a9646d5f210a15ed7a2
kihyuny <103742790+kihyuny@users.noreply.github.com>
2023-04-25 18:50:03 +0900

    [Silver III] Title: 수리공 항승, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -0,0 +1,18 @@
+N, L = map(int,input().split())
+hole = list(map(int,input().split()))
+hole.sort()
+pipe = ['x' for _ in range(1000)]
+for idx in hole:
+    pipe[idx-1]='o'
+cut=0
+for idx in range(len(pipe)):
+    if pipe[idx] == 'o':
+        cut += 1
+        for patch in range(idx, idx+L):
+            if patch < len(pipe):
+                pipe[patch] = 'x'
+            else:
+                break
+
+
+print(cut)
