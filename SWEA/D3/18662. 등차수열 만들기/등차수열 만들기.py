8064149ba8d630090d908dc2bdde054bad6245f5
kihyun2 <103742790+kihyun2@users.noreply.github.com>
2023-11-02 17:50:18 +0900

    [D3] Title: 등차수열 만들기, Time: 796 ms, Memory: 59,812 KB -BaekjoonHub

--- SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.py
+++ SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.py
@@ -1,10 +1,111 @@
-T = int(input())
 
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    a,b,c = map(int,input().split())
-    dif_left = b-a
-    dif_right = c-b
-    if dif_left == dif_right:
-        print(f'#{test_case} {0.0}')
-    else:
-        print(f"#{test_case} {abs((dif_right) - (dif_left)) / 2}")
+
+    arry = list(map(int, input().split()))
+    
+    plus = 1001.0
+    minus = 1001.0
+
+    
+    # 더하기
+    for idx, val in enumerate(arry):
+        x = 0.0
+        cd2 = arry[2]-arry[1]
+        cd1 = arry[1]-arry[0]
+        if cd1 == cd2:
+            plus, minus = 0.0, 0.0
+            break
+        if idx == 0:
+            while cd1 > cd2:
+                val += 0.1
+                val = round(val,1)
+                cd1 = arry[1] - val
+                x += 0.1
+                x = round(x,1)
+                if cd1 == cd2:
+                    if x < plus:
+                        plus = x
+                    break
+        if idx == 1:
+            before, after = cd2 - cd1, cd2 - cd1
+            while abs(before) >= abs(after):
+                before = round(cd2 - cd1,1)
+                val += 0.1
+                val = round(val,1)
+                cd1 = val - arry[0]
+                cd1 = round(cd1, 1)
+                cd2 = arry[2] - val
+                cd2 = round(cd2, 1)
+                after = round(cd2 - cd1,1)
+                x += 0.1
+                x = round(x,1)
+                if cd1 == cd2:
+                    if x < plus:
+                        plus = x
+                    break
+        if idx == 2:
+            while cd1 > cd2:
+                val += 0.1
+                val = round(val,1)
+                cd2 = val - arry[1]
+                x += 0.1
+                x = round(x,1)
+                if cd1 == cd2:
+                    if x < plus:
+                        plus = x
+                    break
+    
+    # 빼기
+    for idx, val in enumerate(arry):
+        x = 0.0
+        cd1 = arry[1]-arry[0]
+        cd2 = arry[2]-arry[1]
+        if cd1 == cd2:
+            minus = x
+            break
+        if idx == 0:
+            while cd1 < cd2:
+                val -= 0.1
+                val = round(val,1)
+                cd1 = arry[1] - val
+                x += 0.1
+                x = round(x,1)
+                if cd1 == cd2:
+                    if x < minus:
+                        minus = x
+                    break
+        if idx == 1:
+            before, after = cd2 - cd1, cd2 - cd1
+            while abs(before) >= abs(after):
+                before = round(cd2 - cd1,1)
+                val -= 0.1
+                val = round(val, 1)
+                cd1 = val - arry[0]
+                cd1 = round(cd1, 1)
+                cd2 = arry[2] - val
+                cd2 = round(cd2, 1)
+                after = round(cd2 - cd1,1)
+                x += 0.1
+                x = round(x,1)
+                if cd1 == cd2:
+                    if x < minus:
+                        minus = x
+                    break
+        if idx == 2:
+            while cd1 < cd2:
+                val -= 0.1
+                val = round(val,1)
+                cd2 = val - arry[1]
+                x += 0.1
+                x = round(x,1)
+                if cd1 == cd2:
+                    if x < minus:
+                        minus = x
+                    break
+        
+
+    minus = round(minus,1)
+    plus = round(plus,1)
+    print(f"#{test_case} {min(minus,plus)}")
