7c5267c4600f0973000af4216c4cccdc7f340403
kihyun2 <103742790+kihyun2@users.noreply.github.com>
2023-11-10 20:45:36 +0900

    [D3] Title: 농작물 수확하기, Time: 161 ms, Memory: 60,708 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -0,0 +1,30 @@
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for tc in range(1, T + 1):
+    # 입력단
+    N = int(input())
+    farm = [[] for i in range(N)]
+    for y in range(N):
+        tmp = input()
+        for idx in range(N):
+            farm[y].append(int(tmp[idx]))
+    
+    # 사용할 변수 선언
+    half = N//2
+    result = farm[0][half]
+    opposite = half
+
+    # 마름모 참조
+    for row in range(1,N):
+        
+        if row <= half:
+            result += sum(farm[row][half-row:half+row+1])
+        
+        if row > half:
+            opposite -= 1
+            result += sum(farm[row][half-opposite:half+opposite+1])
+
+    
+    print(f"#{tc} {result}")
+
