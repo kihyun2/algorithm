84fb736d698ed16da027f22ec96703c12de26b99
kihyun2 <103742790+kihyun2@users.noreply.github.com>
2023-11-10 20:57:39 +0900

    [D3] Title: 농작물 수확하기, Time: 192 ms, Memory: 61,720 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,21 +1,34 @@
+
 T = int(input())
-
-for tc in range(1, T+1):
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for tc in range(1, T + 1):
+    # 입력단
     N = int(input())
-    a = [list(map(int, input())) for _ in range(N)]
-    ans = 0  # output 변수
+    farm = [list(map(int,input())) for _ in range(N)]
+    '''
+    farm = [[] for i in range(N)]
+    for y in range(N):
+        tmp = input()
+        for idx in range(N):
+            farm[y].append(int(tmp[idx]))
+    '''
 
-    # s: 시작포인트, e: 끝포인트
-    s, e = N // 2, N // 2
-    for i in range(N):
-        for j in range(s, e+1):
-            ans += a[i][j]
-        # 행의 인덱스가 mid 전까지는 s-e 간격 늘리고 mid 이후로는 간격 줄임
-        if i < N // 2:
-            s -= 1
-            e += 1
-        else:
-            s += 1
-            e -= 1
+    
+    # 사용할 변수 선언
+    half = N//2
+    result = 0
+    opposite = half
 
-    print("#{} {}".format(tc, ans))
+    # 마름모 참조
+    for row in range(0,N):
+        
+        if row <= half:
+            result += sum(farm[row][half-row:half+row+1])
+        
+        if row > half:
+            opposite -= 1
+            result += sum(farm[row][half-opposite:half+opposite+1])
+
+    
+    print(f"#{tc} {result}")
+
