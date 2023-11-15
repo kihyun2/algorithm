7d28f1c43db5bce2b918feab869c3f2f500b298d
kihyun2 <103742790+kihyun2@users.noreply.github.com>
2023-11-15 18:57:30 +0900

    [D2] Title: 파스칼의 삼각형, Time: 96 ms, Memory: 42,440 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,26 +1,16 @@
 T = int(input())
 
 for tc in range(1, T+1):
-    n = int(input())
-    nums=[]
-    temp = []
-    temp2 = []
-    for i in range(1, n+1):
-        temp2=[]
-        if i > 1:
-            temp2.append(1)
-            for j in range(len(nums)-1):
-                temp_num = nums[j] + nums[j+1]
-                temp2.append(temp_num)
-            temp2.append(1)
-            temp.append(temp2)
-                
-        else:
-            temp.append([1])
-        nums = temp2
-        
+
+    N = int(input())
+    pascal = [[] for _ in range(N)]
+    pascal[0].append(1)
+    for i in range(1, N):
+        pascal[i].append(pascal[i-1][0])
+        for a in range(i-1):
+            pascal[i].append(pascal[i-1][a]+pascal[i-1][a+1])
+        pascal[i].append(pascal[i-1][-1])
+
     print(f'#{tc}')
-    for ele in temp:
-        for mele in ele:
-            print(mele, end=' ')
-        print()
+    for row in pascal:
+        print(*row)
