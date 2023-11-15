ff0ca89dbcf763e3a3e1ab67d112def72e4a341f
kihyun2 <103742790+kihyun2@users.noreply.github.com>
2023-11-15 17:47:48 +0900

    [D2] Title: 간단한 369게임, Time: 97 ms, Memory: 42,444 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -1,22 +1,10 @@
-T = int(input())
+N = int(input())
+lst = list(map(str,[x for x in range(1, N+1)]))
 
-    
-samyogu=['3', '6', '9']
-lst=[list(map(str,str(x))) for x in range(1,T+1)]
+for idx, val in enumerate(lst):
+    if val.count('3') + val.count('6') + val.count('9'):
+        jjak = val.count('3') + val.count('6') + val.count('9')
 
+        lst[idx] = '-' * jjak
 
-
-for idx in range(len(lst)):
-    tmp=0
-    for el in lst[idx]:
-        if el in samyogu:
-            tmp+=1
-    if tmp > 0:
-        lst[idx]='-'*tmp
-
-for idx in range(len(lst)):
-    if idx != 0:
-        print(' ',end='')
-    for idxe in range(len(lst[idx])):
-        print(lst[idx][idxe], end='')
-
+print(*lst)
