9c007a44727d66977cfa82468b3d507cd0b9d7b9
kihyun2 <103742790+kihyun2@users.noreply.github.com>
2023-11-17 23:30:14 +0900

    [D2] Title: 두 개의 숫자열, Time: 103 ms, Memory: 45,060 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -4,25 +4,19 @@ for tc in range(1, T+1):
     N, M = map(int, input().split())
     A = list(map(int, input().split()))
     B = list(map(int, input().split()))
-    lst = []
     result = 0
 
 
     if N > M:
-        longN = A
-        shortN = B
-    else:
-        longN = B
-        shortN = A
+        A, B = B, A
 
-    for start in range(len(longN)-len(shortN)+1):
+    for start in range(len(B)-len(A)+1):
         tmp = 0
-        for i in range(len(shortN)):
-            tmp += longN[start+i]*shortN[i]
-        lst.append(tmp)
+        
+        for i in range(len(A)):
+            tmp += B[start+i]*A[i]
+        
+        if tmp > result:
+            result = tmp
 
-    for i in lst:
-        if result < i:
-            result = i
-            
     print(f'#{tc} {result}')
