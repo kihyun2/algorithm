e26c7c30bd05b32800c327930008ccf26a2620ff
kihyun2 <103742790+kihyun2@users.noreply.github.com>
2023-11-17 18:34:20 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 133 ms, Memory: 56,992 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -2,10 +2,9 @@ for tc in range(1,11):
     N = int(input())
     lst = list(map(int,input().split()))
 
-    # 100 * 100 * 1000 = 10^7    
     for _ in range(N):
         lst[lst.index(max(lst))] -= 1
         lst[lst.index(min(lst))] += 1
-    
+
     result = lst[lst.index(max(lst))] - lst[lst.index(min(lst))]
     print(f'#{tc} {result}')
