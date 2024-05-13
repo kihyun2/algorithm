665a3e90dc133a70e2e614cf8ad64f01125f1bc6
kihyun2 <103742790+kihyun2@users.noreply.github.com>
2024-05-13 21:19:16 +0900

    [D3] Title: 합성수 방정식, Time: 107 ms, Memory: 47,720 KB -BaekjoonHub

---
+++ SWEA/D3/16002. 합성수 방정식/README.md
@@ -0,0 +1,15 @@
+# [D3] 합성수 방정식 - 16002 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYYAGCNKPgIDFARc) 
+
+### 성능 요약
+
+메모리: 47,720 KB, 시간: 107 ms, 코드길이: 506 Bytes
+
+### 제출 일자
+
+2024-05-13 21:19
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/16002. 합성수 방정식/합성수 방정식.py
@@ -0,0 +1,24 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    result = 0
+    N = int(input())
+
+    for y in range(N+1, (10**9)-(N+1)):
+        bxy = 0
+        x = y + N
+        for i in range(2, int(y**0.5)+1):
+            if y % i == 0:
+                bxy += 1
+                break
+        for i in range(2, int(x**0.5)+1):
+            if x % i == 0:
+                bxy += 1
+                break
+        if bxy == 2:
+            result = x
+            break
+
+                
+
+    print(f'#{tc} {result} {result-N}')
