897245ea144665ba4c71ef9a847db416ab6b833f
kihyun2 <103742790+kihyun2@users.noreply.github.com>
2024-05-19 01:55:51 +0900

    [Bronze I] Title: 달팽이2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1952. 달팽이2/달팽이2.py
+++ 백준/Bronze/1952. 달팽이2/달팽이2.py
@@ -6,30 +6,18 @@ dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
 
-cnt = 0
 i = 0
-nr, nc = 0,0
+r = c = 0
 for _ in range(M*N - 1):
-    nr += dr[i%4]
-    nc += dc[i%4]   
-    if nr >= M or nr < 0 or nc >= N or nc < 0:
-        nr -= dr[i%4]
-        nc -= dc[i%4]
+    nr = r + dr[i%4]
+    nc = c + dc[i%4]   
+    if nr >= M or nr < 0 or nc >= N or nc < 0 or lst[nr][nc]:
         i += 1
-        nr += dr[i%4]
-        nc += dc[i%4]  
-    else:
-        if lst[nr][nc]:
-            nr -= dr[i%4]
-            nc -= dc[i%4]
-            i += 1
-            nr += dr[i%4]
-            nc += dc[i%4]  
-    lst[nr][nc] = True
-        
-    
+        nr = r + dr[i%4]
+        nc = c + dc[i%4]   
 
+    r, c = nr, nc
+    lst[r][c] = True
         
-
 
 print(i)
