a48f4f50e06a6c613de28070e280383e405eb056
kihyun2 <103742790+kihyun2@users.noreply.github.com>
2024-10-21 10:08:21 +0900

    [D2] Title: 파리 퇴치, Time: 116 ms, Memory: 45,056 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,18 +1,18 @@
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    # ///////////////////////////////////////////////////////////////////////////////////
-    
-    N, M = map(int,input().split())
-    fly = list()
+
+    N,M = map(int, input().split())
+    lst = list()
     for _ in range(N):
-        fly.append(list(map(int,input().split())))
-    dead = list()
-    for col in range(N-(M-1)):
-        for row in range(N-(M-1)):
+        lst.append(list(map(int, input().split())))
+
+    result = list()
+    for lr in range(N-M+1):
+        for lc in range(N-M+1):
             tmp = 0
-            for c in range(M):
-                for r in range(M):
-                    tmp += fly[col+c][row+r]
-            dead.append(tmp)
-    print(f"#{test_case} {max(dead)}")
+            for sr in range(M):
+                for sc in range(M):
+                    tmp += lst[lr+sr][lc+sc]
+            result.append(tmp)
+
+    print(f"#{test_case} {max(result)}")
